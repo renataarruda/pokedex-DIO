79b8c54d0fb303e222673f663f8b03de4d44dad9
Renata Arruda <renata.arruda85@gmail.com>
2023-04-28 18:44:57 -0300

    feat: fazendo múltiplas requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,21 @@
+function convertPokemonTypesToList(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToHTML(pokemon) {
     return `
 
     <li class="pokemon">
-          <span class="number">#001</span>
+          <span class="number">#${pokemon.id}</span>
           <span class="name">${pokemon.name}</span>
 
           <div class="detail">
             <ol class="types">
-              <li class="type">grass</li>
-              <li class="type">poison</li>
+                ${convertPokemonTypesToList(pokemon.types).join('')}
             </ol>
 
             <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+              src="${pokemon.sprites.other.dream_world.front_default}"
               alt="${pokemon.name}"
             />
           </div>
