20febf958085772cde4208d524f7cb5834394629
Renata Arruda <renata.arruda85@gmail.com>
2023-05-03 20:17:12 -0300

    fix: corrigindo requisição de pokemons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -31,5 +31,4 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
-    .catch((error) => console.log(error))
 }
