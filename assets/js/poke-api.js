7a46f52b260fda1489e086e41bddbecaf2b12266
Renata Arruda <renata.arruda85@gmail.com>
2023-05-03 19:51:14 -0300

    fix: fix code review

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -6,7 +6,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.name = pokeDetail.name
 
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const {type} = types
+    const [type] = types
 
     pokemon.types = types
     pokemon.type = type
@@ -22,7 +22,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
     .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
     return fetch(url)
